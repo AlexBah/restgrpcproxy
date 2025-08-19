baa8d055639edf45421cb3a31b3797174d844296
Alex Bah <P_osts@mail.ru>
2025-08-19 23:19:30 +0700

    GRPC connection #1

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,27 +1,51 @@
 package handler
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 	"restgrpcproxy/internal/lib/logger/sl"
 
+	gw "restgrpcproxy/gen/go/sso"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"golang.org/x/exp/slog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 // collects incoming request and sends it to the output
 func HandlerReturn(w http.ResponseWriter, r *http.Request, gRPCServer string, log *slog.Logger) {
 	op := "restgrpcproxy.handler.HandlerReturn"
 
-	requestString := fmt.Sprintf("%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		requestString += fmt.Sprintf("%q : %q\n", k, v)
+	ctx := context.Background()
+	mux := runtime.NewServeMux()
+
+	/*
+		creds, err := credentials.NewClientTLSFromFile("path/to/server-cert.pem", "")
+		if err != nil {
+			log.Error("failed to load TLS credentials", sl.Err(err))
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		opts := []grpc.DialOption{
+			grpc.WithTransportCredentials(creds),
+			// without TLS:
+			// grpc.WithTransportCredentials(insecure.NewCredentials()),
+		}
+	*/
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	}
-	requestString += fmt.Sprintf("\nHost = %q\nRemoteAddr = %q\n\n", r.Host, r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Error(op, sl.Err(err))
+
+	err := gw.RegisterAuthHandlerFromEndpoint(ctx, mux, gRPCServer, opts)
+	if err != nil {
+		log.Error(op, "failed to register gateway", sl.Err(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
 	}
-	for k, v := range r.Form {
-		requestString += fmt.Sprintf("Form[%q] = %q\n", k, v)
-	}
-	fmt.Fprintf(w, "%s", requestString)
+
+	// Обслуживаем запрос через gateway
+	mux.ServeHTTP(w, r)
+
 }
